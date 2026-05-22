6030fe4d5caf0db3cd35b839710fe4bb364c285c
ZXH2y <andri9825559@gmail.com>
2026-05-22 17:34:13 +0800

    SetUp Migration

--- database/migrations/2026_05_21_074807_create_human_resources_app.php
+++ database/migrations/2026_05_21_074807_create_human_resources_app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\Eloquent\SoftDeletes;   
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,10 +12,86 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('human_resources_app', function (Blueprint $table) {
-            $table->id();
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();   
+            $table->string('fullname');
+            $table->string('email')->unique();
+            $table->string('phone_number');
+            $table->string('address');
+            $table->date('birth_date');
+            $table->date('hire_date');
+            $table->foreignKey('departement_id')->constrained('departements');
+            $table->foreignKey('role_id')->constrained('roles');
+            $table->string('satatus');
+            $table->decimal('salary', 10,2);
             $table->timestamps();
+            $table->softDeletes();
+
         });
+
+
+        Schema::create('departements', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->foreignKey('assigned_to')->constrained('employees');
+            $table->date('due_date');
+            $table->string('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+        Schema::create('payroll', function (Blueprint $table) {
+            $table->id();
+            $table->foreignKey('employee_id')->constrained('employees');
+            $table->decimal('salary', 10, 2);
+            $table->decimal('bonuses', 10, 2)->nullable();
+            $table->decimal('deductions', 10, 2)->nullable();
+            $table->decimal('net_salary', 10, 2);
+            $table->date('pay_date');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+        
+        Schema::create('presences', function (Blueprint $table) {
+            $table->id();
+            $table->foreignKey('employee_id')->constrained('employees');
+            $table->date('check_id');
+            $table->date('check_out');
+            $table->date('date');
+            $table->string('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('leave_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignKey('employee_id')->constrained('employees');
+            $table->string('leave_type');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+
     }
 
     /**
@@ -22,6 +99,12 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('human_resources_app');
+        Schema::dropIfExists('employees');
+        Schema::dropIfExists('departements');
+        Schema::dropIfExists('roles');
+        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('payroll');
+        Schema::dropIfExists('presences');
+        Schema::dropIfExists('leave_requests');
     }
 };
